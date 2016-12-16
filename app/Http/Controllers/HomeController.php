7d9a4c1b16e3f7df84a27045b2b06dec41c6338b
Chantouch <chantouchsek.cs83@gmail.com>
2016-12-16 16:10:49 +0700

    -test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,6 +27,7 @@ class HomeController extends Controller
         return view('admin.dashboard', compact('title'));
     }
 
+
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
